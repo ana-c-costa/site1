c395e28897e2e0e2d7f49e5ac1b07ebd397921fa
camila-reinert <camila_f_reinert@estudante.sesisenai.org.br>
2023-05-31 12:12:04 -0300

    site

---
+++ verifica.php
@@ -0,0 +1,27 @@
+<?php 
+    include("conecta.php");
+    include("crud.php");
+
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+
+
+    $n = 0;
+    $m = 0;
+    while($linhas = $comando->fetch())
+    {
+        $n = 1;
+        $m = 1;
+    }
+    if($n == 0)
+    {
+        header("Location: login.html");
+    }
+    if($n == 1)
+    {  
+        else
+        {
+            header("Location: telainicial.html");
+        }
+    }
+?>
