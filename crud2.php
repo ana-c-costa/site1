8b84565206fedcd615de1bf38392a7a0ddb4ad1f
camila-reinert <camila_f_reinert@estudante.sesisenai.org.br>
2023-06-06 10:42:44 -0300

    site

--- crud2.php
+++ crud2.php
@@ -4,7 +4,7 @@ include("conecta2.php");
     $nome = $_POST["nome"]; 
     $email = $_POST["email"];
     $senha = $_POST["senha"];
-    $conf_senha = $_POST["conf_senha"]
+    $conf_senha = $_POST["conf_senha"];
 
 
 $comando = $pdo->prepare("INSERT INTO cadastro VALUES ('$nome', '$email', '$senha', '$conf_senha')");
